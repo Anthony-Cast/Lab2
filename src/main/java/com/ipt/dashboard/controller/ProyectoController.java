c1605843e72767f77149c1cf9e64dd951bcf9013
LiziAC05 <lizbeth_aqcast@hotmail.com>
2021-04-19 20:30:33 -0500

    --

--- src/main/java/com/ipt/dashboard/controller/ProyectoController.java
+++ src/main/java/com/ipt/dashboard/controller/ProyectoController.java
@@ -1,7 +1,24 @@
 package com.ipt.dashboard.controller;
 
+import com.ipt.dashboard.repository.ProyectoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/proyecto")
 public class ProyectoController {
+    @Autowired
+    ProyectoRepository proyectoRepository;
+    @GetMapping(value = {"/listar", " "})
+    public String listarProyectos(Model model){
+        model.addAttribute("listaProyectos", proyectoRepository.findAll());
+        return "/proyecto/lista";
+    }
+    @GetMapping("/nuevo")
+    public String nuevoProyecto(){
+        return "/proyecto/nuevo";
+    }
 }
