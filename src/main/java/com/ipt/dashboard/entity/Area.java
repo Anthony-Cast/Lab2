c9cb2791f84d5693220a1bc776c4aa21dce6d752
joseluismunozzuta <joseluismunozzuta@gmail.com>
2021-04-19 20:29:58 -0500

    Commit 2 - Avance CRUD Areas

--- src/main/java/com/ipt/dashboard/entity/Area.java
+++ src/main/java/com/ipt/dashboard/entity/Area.java
@@ -1,9 +1,30 @@
 package com.ipt.dashboard.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="areas")
 public class Area {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idarea;
+    @Column(nullable = false)
+    private String nombrearea;
+
+    public int getIdarea() {
+        return idarea;
+    }
+
+    public void setIdarea(int idarea) {
+        this.idarea = idarea;
+    }
+
+    public String getNombrearea() {
+        return nombrearea;
+    }
+
+    public void setNombrearea(String nombrearea) {
+        this.nombrearea = nombrearea;
+    }
 }
---
+++ src/main/java/com/ipt/dashboard/repository/AreaRepository.java
@@ -0,0 +1,8 @@
+package com.ipt.dashboard.repository;
+
+import com.ipt.dashboard.entity.Area;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AreaRepository extends JpaRepository<Area,Integer> {
+
+}
