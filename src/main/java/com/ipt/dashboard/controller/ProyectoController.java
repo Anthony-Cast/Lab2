f725d8577e656768214f8744fe8827670477bdda
LiziAC05 <lizbeth_aqcast@hotmail.com>
2021-04-19 23:04:37 -0500

    cambios

--- src/main/java/com/ipt/dashboard/controller/ProyectoController.java
+++ src/main/java/com/ipt/dashboard/controller/ProyectoController.java
@@ -1,10 +1,12 @@
 package com.ipt.dashboard.controller;
 
+import com.ipt.dashboard.entity.Proyecto;
 import com.ipt.dashboard.repository.ProyectoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +22,9 @@ public class ProyectoController {
     @GetMapping("/nuevo")
     public String nuevoProyecto(){
         return "/proyecto/nuevo";
+    }
+    @PostMapping("/guardar")
+    public String guardarProyecto(Proyecto proyecto){
+
     }
 }
